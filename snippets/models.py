1c3eb768d90b6944fc7e30a1971458056b35dad6
Appz <satheeshappz@gmail.com>
2025-08-30 02:02:16 +0530

    feat(models): add Snippet and Tag models

---
+++ snippets/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.conf import settings
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Snippet(models.Model):
+    title = models.CharField(max_length=200)
+    note = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="snippets"
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="snippets",
+        blank=True
+    )
+
+    def __str__(self):
+        return self.title
